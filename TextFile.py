7acb71d9e4af73cc919bd2f21103ae96bc211127
KrutovaSV <mira.kru@yandex.ru>
2024-06-20 17:27:40 +0300

    20.06

--- TextFile.py
+++ TextFile.py
@@ -1,6 +1,9 @@
-about_me=input()
-about_me_2=about_me.split()
-file_about_me_2=open('file_about_me_2','a')
-file_about_me_2.writelines(about_me_2)
-file_about_me_2.close()
-print(about_me_2)
+# about_me=input()
+# array_about_me=about_me.split()
+# file_about_me=open("file_about_me","a")
+# file_about_me.writelines(array_about_me)
+# file_about_me.close()
+
+with open('file_about_2.txt','w') as data:
+    data.writelines('dick1\n')
+    data.writelines('dick2\n')
